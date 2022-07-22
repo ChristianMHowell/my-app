c0b25fa9c927da0ef174ea332d7c694453fe6268
Christian Howell <christian.howell@infosys.com>
2022-07-22 16:50:52 -0700

    Initial commit

---
+++ src/store/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export interface IStoreProps{
+
+}
+export const mainStore: React.FunctionComponent<IStoreProps> = props => {
+
+    return (
+        <React.Fragment>
+
+            
+        </React.Fragment>
+    )
+}
