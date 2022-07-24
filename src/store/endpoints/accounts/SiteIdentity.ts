6a4b3d40a0d8bd9d9bbdd88212ff895998f49bd4
Christian Howell <christian.howell@infosys.com>
2022-07-23 17:03:32 -0700

    PreImage

---
+++ src/store/endpoints/accounts/SiteIdentity.ts
@@ -0,0 +1,34 @@
+import React from "react";
+
+/*
+*
+*
+*
+*
+*
+*/
+interface ISiteIdentityProps {
+    givenName: string,
+    middleName: string,
+    surName: string,
+    streetAddress: string,
+    unitNumber: number,
+    homeCity: string,
+    homeState: string,
+    homeCountry: string,
+    emailAddress: string,
+    phoneNumber: string,
+
+
+}
+/*
+*
+*
+*
+*
+*
+*/
+export const SiteIdentity: React.FunctionComponent<ISiteIdentityProps> = props => {
+
+    return ()
+}
---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+
+export interface IMainStoreProps {
+
+}
+
+export const MainStore: React.FunctionComponent<IMainStoreProps> = props => {
+
+
+    return (
+
+    )
+}
+export default MainStore
